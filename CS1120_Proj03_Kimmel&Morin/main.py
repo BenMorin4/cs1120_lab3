4b97ce0ff7b28e823a22f4763c3bc74c2483f193
tkimmel04 <150847069+tkimmel04@users.noreply.github.com>
2024-02-14 22:58:03 -0500

    Update main.py
    Removed excess comments.

--- CS1120_Proj03_Kimmel&Morin/main.py
+++ CS1120_Proj03_Kimmel&Morin/main.py
@@ -9,14 +9,14 @@ from boggle import (BoggleBoard)
 
 def main():
     seed = int(input("Enter the seed: ")) # Prompt the user to enter the seed for randomization
-    game = BoggleBoard(seed)  # Create an instance of the BoggleBoard class with the specified seed
+    game = BoggleBoard(seed)  
     game.print_board()  # Print the initial Boggle board
 
     while True:
-        word = input ("Enter word (in UPPERcase) or 'quit' to exit: ").upper()  # Prompt the user to enter a word or 'quit' to exit
+        word = input ("Enter word (in UPPERcase) or 'quit' to exit: ").upper()  
         if word == 'QUIT':
             break  # Exit the loop if the user inputs 'quit'
-        game.guess_word(word) # Attempt to find the entered word on the Boggle board
+        game.guess_word(word) 
 
 
 if __name__ == "__main__":
